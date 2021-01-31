d24c7f264232deb872d6c969f2e6f68f56d15d4e
Elite821 <69848414+Elite821@users.noreply.github.com>
2021-01-31 13:49:26 +0700

    Update pytest.py

--- pytest.py
+++ pytest.py
@@ -1,6 +1,21 @@
-import Time
-time = 0
+import time
+tim1= 0
+tim2= 0
+min1= 0
+min2 = 0
 while True:
-  time = time + 1
-  time.sleep(1)
-  print(time)
+    tim1 = tim1 + 1
+    time.sleep(1)
+    if tim1 == 10:
+        tim1 = 0
+        tim2 = tim2 + 1
+    if tim2 == 6:
+        min1 = min1 + 1
+        tim2 = 0
+        tim1 = 0
+    if min1 == 6:
+        min2 = min2 + 1
+        min1 = 0
+        tim1 = 0
+        tim2 = 0
+    print(min2,min1,":",tim2,tim1)
